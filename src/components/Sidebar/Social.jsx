2a8f7f80950c8c689a19bdfeb61acfc750c425ac
equaliser0 <accounts@equaliser.minimalcity.de>
2021-12-28 16:25:44 +0100

    Add social Links

--- src/components/Sidebar/Social.jsx
+++ src/components/Sidebar/Social.jsx
@@ -1,25 +1,25 @@
 import { SvgIcon, Link } from "@material-ui/core";
 import { ReactComponent as GitHub } from "../../assets/icons/github.svg";
-import { ReactComponent as Medium } from "../../assets/icons/medium.svg";
+import { ReactComponent as Linkedin } from "../../assets/icons/linkedin.svg";
 import { ReactComponent as Twitter } from "../../assets/icons/twitter.svg";
 import { ReactComponent as Discord } from "../../assets/icons/discord.svg";
 
 export default function Social() {
   return (
     <div className="social-row">
-      <Link href="https://github.com/OlympusDAO" target="_blank">
+      <Link href="https://github.com/cunoro" target="_blank">
         <SvgIcon color="primary" component={GitHub} />
       </Link>
 
-      <Link href="https://olympusdao.medium.com/" target="_blank">
-        <SvgIcon color="primary" component={Medium} />
+      <Link href="https://www.linkedin.com/company/cunoro/" target="_blank">
+        <SvgIcon color="primary" component={Linkedin} />
       </Link>
 
-      <Link href="https://twitter.com/OlympusDAO" target="_blank">
+      <Link href="https://twitter.com/cunorofinance" target="_blank">
         <SvgIcon color="primary" component={Twitter} />
       </Link>
 
-      <Link href="https://discord.gg/6QjjtUcfM4" target="_blank">
+      <Link href="hhttps://discord.gg/7uP2SQ2t5F" target="_blank">
         <SvgIcon color="primary" component={Discord} />
       </Link>
     </div>
