b43776b303f24102d76072a87910f6c24e3369e9
equaliser0 <accounts@equaliser.minimalcity.de>
2022-06-22 21:27:15 +0200

    update addresses

--- src/helpers/bond/index.ts
+++ src/helpers/bond/index.ts
@@ -21,11 +21,11 @@ export const bend = new CustomBond({
     networkAddrs: {
         [Networks.AVAX_MAIN]: {
             bondAddress: addressesAvax.BONDEPOSITORY_BEND,
-            reserveAddress: "0x130966628846BFd36ff31a822705796e8cb8C18D",
+            reserveAddress: addressesAvax.BEND_ADDRESS,
         },
         [Networks.AVAX_TEST]: {
             bondAddress: addressesFuji.BONDEPOSITORY_BEND,
-            reserveAddress: "0x19a1165A79AFAAeFd805969B32a0640d4Db9f131",
+            reserveAddress: addressesFuji.BEND_ADDRESS,
         },
     },
     reserveDecimal: 3,
@@ -42,11 +42,11 @@ export const wavax = new CustomBond({
     networkAddrs: {
         [Networks.AVAX_MAIN]: {
             bondAddress: addressesAvax.BONDEPOSITORY_AVAX,
-            reserveAddress: "0xb31f66aa3c1e785363f0875a1b74e27b85fd66c7",
+            reserveAddress: addressesAvax.WAVAX_ADDRESS,
         },
         [Networks.AVAX_TEST]: {
             bondAddress: addressesFuji.BONDEPOSITORY_AVAX,
-            reserveAddress: "0xd00ae08403B9bbb9124bB305C09058E32C39A48c",
+            reserveAddress: addressesFuji.WAVAX_ADDRESS,
         },
     },
     reserveDecimal: 18,
