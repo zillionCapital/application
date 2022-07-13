aea3ee05ed79c083434a43c89d3cc068dacc2a71
equaliser0 <accounts@equaliser.minimalcity.de>
2022-07-13 18:18:18 +0200

    update market price

--- src/store/slices/app-slice.ts
+++ src/store/slices/app-slice.ts
@@ -17,7 +17,7 @@ export const loadAppDetails = createAsyncThunk(
     "app/loadAppDetails",
     //@ts-ignore
     async ({ networkID, provider }: ILoadAppDetails) => {
-        const mimPrice = getTokenPrice("MIM");
+        const bendPrice = getTokenPrice("BEND");
         const addresses = getAddresses(networkID);
 
         const stakingContract = new ethers.Contract(addresses.STAKING_ADDRESS, StakingContract, provider);
@@ -26,13 +26,13 @@ export const loadAppDetails = createAsyncThunk(
         const ssabContract = new ethers.Contract(addresses.SNORO_ADDRESS, SSabTokenContract, provider);
         const sabContract = new ethers.Contract(addresses.NORO_ADDRESS, SabTokenContract, provider);
 
-        const marketPrice = 5; //((await getMarketPrice(networkID, provider)) / Math.pow(10, 9)) * mimPrice;
+        // const marketPrice = await getMarketPrice(networkID, provider); // ((await getMarketPrice(networkID, provider)) / Math.pow(10, 9)) * (bendPrice * 333333.333);
 
         const totalSupply = (await sabContract.totalSupply()) / Math.pow(10, 9);
         const circSupply = (await ssabContract.circulatingSupply()) / Math.pow(10, 9);
 
-        const stakingTVL = circSupply * marketPrice;
-        const marketCap = totalSupply * marketPrice;
+        // const stakingTVL = circSupply * marketPrice;
+        // const marketCap = totalSupply * marketPrice;
 
         const tokenBalPromises = allBonds.map(bond => bond.getTreasuryBalance(networkID, provider));
         const tokenBalances = await Promise.all(tokenBalPromises);
@@ -48,6 +48,10 @@ export const loadAppDetails = createAsyncThunk(
         const timeSupply = totalSupply - timeAmount;
 
         const rfv = rfvTreasury / timeSupply;
+        const marketPrice = rfv;
+
+        const stakingTVL = circSupply * marketPrice;
+        const marketCap = totalSupply * marketPrice;
 
         const epoch = await stakingContract.epoch();
         const stakingReward = epoch.distribute;
